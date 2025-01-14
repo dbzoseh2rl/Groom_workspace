1bb07a2e155ad55c81c875cdcd77aa915933b1f1
dbzoseh2rl <dbzoseh2rl123@naver.com>
2025-01-14 10:38:28 +0900

    feat: project setting add

---
+++ src/main/java/com/example/groom_workspace/GroomWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.groom_workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomWorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroomWorkspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/groom_workspace/GroomWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groom_workspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomWorkspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
